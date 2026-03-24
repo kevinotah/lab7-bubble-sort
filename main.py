b06059b5e6b9fc8bde44561df9f59528ecac2dae
Kevin Otah <kevin.ogbusuo@epita.fr>
2026-03-24 20:37:07 +0100

    Trying to visualise the sorting algorithm

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ def bubblesort(arr: list[int]) -> list[int]:
     
     i = 0
     while i < len(arr):
+
         for j in range(len(arr) - 1 - i):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
         i += 1
-    
+        
     return arr
+
+bubblesort([3, 4, 0, 2, 1, 5])
